67d445a865be430af61f87548ca5f9e54cc7eec6
fjfdepedro <fjfdepedro@gmail.com>
2013-03-05 12:50:52 +0100

    First

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Ufo::Application.config.secret_token = 'a074c2e2d5d26e8a488dceb3dea3c5e6edccaa5e8b24ee512e522f3a60ef9c891d9853bc435918266dfd127032d27b0e3ac185e8417eb02c012eb78ea6bfc031'
