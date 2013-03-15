d218d6b257d4c7af68c3d8c163a4e0ad5c85c891
fjfdepedro <fjfdepedro@gmail.com>
2013-03-15 11:41:44 +0100

    connect mongo and ufo database

--- app/controllers/sightings_controller.rb
+++ app/controllers/sightings_controller.rb
@@ -1,4 +1,7 @@
 class SightingsController < ApplicationController
   def index
+	    @listaUFO = UfoModel.all.desc(:sighted_at).limit(20)
+        @numUFO = UfoModel.count()
+        #http://two.mongoid.org/docs/querying/finders.html#all
   end
 end
---
+++ app/models/ufo.rb
@@ -0,0 +1,11 @@
+class Ufo < ActiveRecord::Base
+  include Mongoid::Document
+  store_in collection: "ufo"
+
+  field :sighted_at, type: String
+  field :reported_at, type: String
+  field :location, type: String
+  field :shape, type: String
+  field :duration, type: String
+  field :description, type: String 
+end 
