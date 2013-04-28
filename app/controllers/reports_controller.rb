3bbef2e959dcb11c111a68f3f77ab3415f172fc6
fjfdepedro <fjfdepedro@gmail.com>
2013-04-28 04:14:09 +0200

    formulario de alta

--- app/controllers/reports_controller.rb
+++ app/controllers/reports_controller.rb
@@ -1,25 +1,25 @@
 class ReportsController < ApplicationController
   # GET /reports
   # GET /reports.json
-  #def index
+  def index
     #@reports = Report.all.desc(:sighted_at).limit(10)
 
     #respond_to do |format|
-      #format.html # index.html.erb
-      #format.json { render json: @reports }
+    #  format.html # index.html.erb
+    #  format.json { render json: @reports }
     #end
-  #end
+  end
 
   # GET /reports/1
   # GET /reports/1.json
-  #def show
-    #@report = Report.find(params[:id])
+  def show
+    @report = Report.find(params[:id])
 
-    #respond_to do |format|
-      #format.html # show.html.erb
-      #format.json { render json: @report }
-    #end
-  #end
+    respond_to do |format|
+      format.html # show.html.erb
+      format.json { render json: @report }
+    end
+  end
 
   # GET /reports/new
   # GET /reports/new.json
@@ -37,14 +37,19 @@ class ReportsController < ApplicationController
   end
 
   # GET /reports/1/edit
-  def editcontentufo
-    @report = Report.find(params[:id])
+  def edit
+    #@report = Report.find(params[:id])
   end
 
   # POST /reports
   # POST /reports.json
   def create
-    @report = Report.new(params[:report])
+    @numUFO = Report.count()
+
+    @tmp = params[:report]
+    @tmp["links"] = @tmp["links"].values
+
+    @report = Report.new(@tmp)
 
     respond_to do |format|
       if @report.save
@@ -59,29 +64,29 @@ class ReportsController < ApplicationController
 
   # PUT /reports/1
   # PUT /reports/1.json
-  def updatecontentufo
-    @report = Report.find(params[:id])
+  def update
+    #@report = Report.find(params[:id])
 
-    respond_to do |format|
-      if @report.update_attributes(params[:report])
-        format.html { redirect_to @report, notice: 'Ufo model was successfully updated.' }
-        format.json { head :no_content }
-      else
-        format.html { render action: "edit" }
-        format.json { render json: @report.errors, status: :unprocessable_entity }
-      end
-    end
+    #respond_to do |format|
+     # if @report.update_attributes(params[:report])
+       # format.html { redirect_to @report, notice: 'Ufo model was successfully updated.' }
+       # format.json { head :no_content }
+      #else
+      #  format.html { render action: "edit" }
+      #  format.json { render json: @report.errors, status: :unprocessable_entity }
+      #end
+    #end
   end
 
   # DELETE /reports/1
   # DELETE /reports/1.json
-  def destroycontentufo
-    @report = Report.find(params[:id])
-    @report.destroy
+  def destroy
+   # @report = Report.find(params[:id])
+    #@report.destroy
 
-    respond_to do |format|
-      format.html { redirect_to reports_url }
-      format.json { head :no_content }
-    end
+    #respond_to do |format|
+    #  format.html { redirect_to reports_url }
+    #  format.json { head :no_content }
+    #end
   end
 end
