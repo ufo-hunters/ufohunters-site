0d8ead4d8bcb1397e94c2823e42fe29735dca38a
fjfdepedro <fjfdepedro@gmail.com>
2013-06-05 23:21:49 +0200

    email no en environment

--- app/models/notifier.rb
+++ app/models/notifier.rb
@@ -1,14 +1,14 @@
  class Notifier < ActionMailer::Base
 
-    include SendGrid 
+    #include SendGrid 
 
-    default from: "info@ufo-hunters.com" 
+    #default from: "info@ufo-hunters.com" 
     #sendgrid_category :use_subject_lines 
     #sengrid_enable :ganalytics, :opentrack
 
-    def test_notification 
-      mail(:to => "fjfdepedro@gmail.com", :subject => "mail from heroku")
-      puts("Sent mail from notifier") 
-   end 
+    #def test_notification 
+     # mail(:to => "fjfdepedro@gmail.com", :subject => "mail from heroku")
+     # puts("Sent mail from notifier") 
+   #end 
 
   end 
