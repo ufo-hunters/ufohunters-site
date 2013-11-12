36f902224d3af76298abdea454005f54185be72b
paco <paco@paco.(none)>
2013-11-13 00:42:40 +0100

    google+ and title

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -18,7 +18,7 @@ module ApplicationHelper
          rescue => ex
             logger.info "Invalid date - #{ex.class}: #{ex.message}"
             return ""
-         end
+         end 
       end
    end
 
