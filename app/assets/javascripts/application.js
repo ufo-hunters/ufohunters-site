f4d144dafb87421050db29fedb2d76e852e0260a
fjfdepedro <fjfdepedro@gmail.com>
2013-12-29 10:28:00 +0100

    pruebas

--- app/assets/javascripts/application.js
+++ app/assets/javascripts/application.js
@@ -11,9 +11,12 @@
 // GO AFTER THE REQUIRES BELOW.
 //
 
-
 //= require jquery
 //= require jquery_ujs
 //= require prettyphoto-rails 
-//= require twitter/bootstrap
+//= require twitter/bootstrap/bootstrap-transition
+//= require twitter/bootstrap/bootstrap-alert
+//= require twitter/bootstrap/bootstrap-modal
+//= require twitter/bootstrap/bootstrap-button 
+//= require twitter/bootstrap/bootstrap-collapse
 //= require_tree .
