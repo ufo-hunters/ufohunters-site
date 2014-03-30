06ad57aeca6b96bc5b99de7730c04672e4832a24
José Carlos García <jcarlosgarcia@hotmail.com>
2014-03-31 00:43:13 +0200

    Articles list, model, menu...

--- app/helpers/articles_helper.rb
+++ app/helpers/articles_helper.rb
@@ -4,4 +4,24 @@ module ArticlesHelper
   	Report.where(:sighted_at => article.date_filter)
   end
 
+  def self.friendly_title(article)
+    
+    unless article.blank?
+       title = article.title
+       title += "-" + self.format_date(article.published_date) unless article.published_date.blank?
+       title.gsub("'","").gsub("/","-").gsub("&", "-").gsub("?","-").gsub(".","")
+    end
+  end
+
+  def self.format_date date
+    unless date.blank?
+      begin
+        date.to_date.strftime("%Y-%m-%d")
+      rescue => ex
+        logger.info "Invalid date - #{ex.class}: #{ex.message}"
+        return ""
+      end 
+    end               
+  end
+
 end
