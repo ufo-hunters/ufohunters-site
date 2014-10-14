ed540696deb97aa2329e106968937cd7c6f62285
jcarlosgarcia <jcarlosgarcia@hotmail.com>
2014-10-14 20:00:07 +0200

    added Uploader

--- app/uploaders/ckeditor_attachment_file_uploader.rb
+++ app/uploaders/ckeditor_attachment_file_uploader.rb
@@ -1,21 +1,20 @@
 # encoding: utf-8
 class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   include Ckeditor::Backend::CarrierWave
-  include Cloudinary::CarrierWave
 
   # Include RMagick or ImageScience support:
   # include CarrierWave::RMagick
-  include CarrierWave::MiniMagick
+  # include CarrierWave::MiniMagick
   # include CarrierWave::ImageScience
 
   # Choose what kind of storage to use for this uploader:
-  # storage :file
+  storage :file
 
   # Override the directory where uploaded files will be stored.
   # This is a sensible default for uploaders that are meant to be mounted:
-  # def store_dir
-  #  "uploads/ckeditor/attachments/#{model.id}"
-  # end
+  def store_dir
+    "uploads/ckeditor/attachments/#{model.id}"
+  end
 
   # Provide a default URL as a default if there hasn't been a file uploaded:
   # def default_url
@@ -28,14 +27,6 @@ class CkeditorAttachmentFileUploader < CarrierWave::Uploader::Base
   # def scale(width, height)
   #   # do something
   # end
-
-  [:extract_content_type, :set_size, :read_dimensions].each do |method|
-    define_method :"#{method}_with_cloudinary" do
-      send(:"#{method}_without_cloudinary") if self.file.is_a?(CarrierWave::SanitizedFile)
-      {}
-    end
-    alias_method_chain method, :cloudinary
-  end
 
   # Add a white list of extensions which are allowed to be uploaded.
   # For images you might use something like this:
