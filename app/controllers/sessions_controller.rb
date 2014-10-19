bebe616e81486cc5018a8bc5af10601e5b0a7d91
fjfdepedro <fjfdepedro@gmail.com>
2014-10-20 00:05:15 +0200

    form sign in and sign up

--- app/controllers/sessions_controller.rb
+++ app/controllers/sessions_controller.rb
@@ -1,16 +1,29 @@
 class SessionsController < ApplicationController
-  def new
-  end
+	def new
+		@numUFO = Report.where(:status => 1).count()
+	    @menu = "uforesearch"
+	    @articles = Article.all.desc(:published_date)
+	    @page_title = "UFO Resarch Team - Articles"
+	    @page_description = "Latest Articles"
+	    @user = User.new
+
+	    respond_to do |format|
+	      	format.html # index.html.erb
+	      	format.json { render json: @articles }
+    	end
+	end
 
 	def create
+	  @page_title = "UFO Resarch Team - Articles"
+	  @user = User.new
 	  user = User.where(:username => params[:username]).first
 	  if user && user.authenticate(params[:password])
 	    session[:user_id] = user.id
 	    redirect_to :controller => 'articles', :action =>'myspace'
 	  else
-	    flash.now.alert = "Invalid username or password"
-	    redirect_to :controller => 'articles', :action =>'uforesearch'
-
+	  	flash["form"]=1
+	    flash["error"]="Invalid username or password"
+        redirect_to :controller => 'articles', :action =>'uforesearchteam'
 	  end
 	end
 
