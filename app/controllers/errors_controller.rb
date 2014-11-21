a15afc90ee7ca204c82bc773b8b27a4c6df7f924
fjfdepedro <fjfdepedro@gmail.com>
2014-11-21 17:11:49 +0100

    error controller

--- app/controllers/errors_controller.rb
+++ app/controllers/errors_controller.rb
@@ -1,14 +1,17 @@
 class ErrorsController < ApplicationController
  
   def not_found
+  	@page_title = "ERROR"
     render :status => 404
   end
  
   def unacceptable
+  	@page_title = "ERROR"
     render :status => 422
   end
  
   def internal_error
+  	@page_title = "ERROR"
     render :status => 500
   end
  
